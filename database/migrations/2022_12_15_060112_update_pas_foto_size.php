b116b0149f5082f9b7b43d903b3d85e7434df79c
Fakhrurrida Clarendia <fakhrurridawidodo@gmail.com>
2022-12-15 13:03:45 +0700

    Create 2022_12_15_060112_update_pas_foto_size.php

---
+++ database/migrations/2022_12_15_060112_update_pas_foto_size.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('rk_permit', function (Blueprint $table) {
+            $table->longText('pas_foto')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('rk_permit', function (Blueprint $table) {
+            $table->string('pas_foto')->change();
+        });
+    }
+};
