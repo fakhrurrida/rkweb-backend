060fff44cc3b5d27e7dd5bb0b6aa05360f9f6e43
Fakhrurrida Clarendia <fakhrurridawidodo@gmail.com>
2022-12-15 11:35:15 +0700

    Create 2022_12_15_042808_create_rk_permit_table.php

---
+++ database/migrations/2022_12_15_042808_create_rk_permit_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rk_permit', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 100);
+            $table->string('nrp', 100);
+            $table->string('sub_kontraktor', 100);
+            $table->string('jabatan', 100);
+            $table->string('pas_foto', 100);
+            $table->string('kompetensi', 100);
+            $table->string('status', 100);
+            $table->date('masa_berlaku', 100);
+            $table->string('aktivitas_terakhir', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rk_permit');
+    }
+};
